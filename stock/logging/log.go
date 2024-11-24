90e85a7f555c1917605f923d3f9e1dd590924417
SwanHtetAungPhyo <swanhtetaungp@gmail.com>
2024-11-24 22:36:13 +0100

    sub module removal

--- stock/logging/log.go
+++ stock/logging/log.go
@@ -3,41 +3,56 @@ package logging
 import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"os"
+
+	"log"
 )
 
 var globalLogger *zap.Logger
 
-func GlobalLogInit(){
-	config := zap.Config{
-		Level: zap.NewAtomicLevelAt(zap.InfoLevel),
-		Development: true,
-		Encoding: "json",
-		EncoderConfig:  zapcore.EncoderConfig{
-			TimeKey: "time",
-			LevelKey: "level",
-			NameKey: "logger",
-			CallerKey: "caller",
-			MessageKey: "msg",
-			StacktraceKey: "stacktrace",
-			LineEnding: zapcore.DefaultLineEnding,
-			EncodeLevel: zapcore.CapitalLevelEncoder,
-			EncodeTime: zapcore.ISO8601TimeEncoder,
-			EncodeDuration: zapcore.StringDurationEncoder,
-			EncodeCaller: zapcore.ShortCallerEncoder,
-		},
-		OutputPaths: []string{"stdout"},
-		ErrorOutputPaths: []string{"stderr"},
-	}	
+func GlobalLogInit() {
 
-	logger, err := config.Build()
-	if err != nil {
-		panic("Failed to init the loggger"+ err.Error())
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:       "time",
+		LevelKey:      "level",
+		NameKey:       "logger",
+		CallerKey:     "caller",
+		MessageKey:    "msg",
+		StacktraceKey: "stacktrace",
+		LineEnding:    zapcore.DefaultLineEnding,
+		EncodeLevel:   zapcore.CapitalColorLevelEncoder, 
+		EncodeTime:    zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeCaller:  zapcore.ShortCallerEncoder,
 	}
-	globalLogger = logger
+	var encoder zapcore.Encoder
+	if os.Getenv("LOG_ENV") == "production" {
+
+		encoder = zapcore.NewJSONEncoder(encoderConfig)
+	} else {
+
+		encoder = zapcore.NewConsoleEncoder(encoderConfig)
+	}
+
+	core := zapcore.NewCore(
+		encoder,
+		zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(os.Stderr)),
+		zap.NewAtomicLevelAt(zap.InfoLevel),
+	)
+
+
+	logger := zap.New(core)
+
+
+	host, err := os.Hostname()
+	if err != nil {
+		log.Fatalf("Error getting hostname: %v", err)
+	}
+
+	globalLogger = logger.With(zap.String("host", host), zap.Int("pid", os.Getpid()))
 
 	defer globalLogger.Sync()
 }
-
-func L() *zap.Logger{
+func L() *zap.Logger {
 	return globalLogger
 }
